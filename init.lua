071a14edb16f8049c53bfb85da6fb1c2a5278c99
multi-nono <multinono123@gmail.com>
2024-08-23 15:59:10 +0200

    Update init.lua
    modification des valeurs je suis toujours pas sure

--- init.lua
+++ init.lua
@@ -2,17 +2,17 @@ local np_terrain = {
 	offset = -11*mult,						
 	scale = 40*mult,
 	spread = {x = 256*mult, y =256*mult, z = 256*mult},
-	seed = 2,
+	seed = 12345,
 	octaves = convex and 1 or 8,
 	persist = 1.00,
 	lacunarity = 1.33,
 }
 
-ocal np_var = {
+local np_var = {
 	offset = 0,						
 	scale = 6*mult,
 	spread = {x = 64*mult, y =64*mult, z = 64*mult},
-	seed = ,
+	seed = 6789,
 	octaves = 4,
 	persist = 0.4,
 	lacunarity = 1.89,
@@ -22,8 +22,7 @@ local np_hills = {
 	offset = 2.5,					
 	scale = -3.5,
 	spread = {x = 64*mult, y =64*mult, z = 64*mult},
---	spread = {x = 32, y =32, z = 32},
-	seed = ,
+	seed = 101112131415,
 	octaves = 3,
 	persist = 0.40,
 	lacunarity = 2.0,
@@ -32,20 +31,20 @@ local np_hills = {
 
 local np_cliffs = {
 	offset = 3,					
-	scale = 0.72,
-	spread = {x = 180*mult, y =180*mult, z = 180*mult},
-	seed = ,
-	octaves = 2,
-	persist = 0.4,
+	scale = 0.702,
+	spread = {x = 180*mult, y =170*mult, z = 180*mult},
+	seed = 1617,
+	octaves = 0.1,
+	persist = 0.05,
 	lacunarity = 2.11,
 }
 
 local np_trees = {
-	offset = - 0.003,
-	scale = 0.008,
+	offset = - 0.03,
+	scale = 0.08,
 	spread = {x = 64, y = 64, z = 64},
-	seed = ,
+	seed = 181920,
 	octaves = 5,
-	persist = 3,
+	persist = 9,
 	lacunarity = 3.21,
 }
